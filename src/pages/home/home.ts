08d528d8fb82cf58f71b3a676abcc20b6f690ecb
Vitor Pola <vitorpola@gmail.com>
2016-12-10 11:37:20 -0200

    'fix'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NewItemModal } from '../prices/new';
+import { NavController, ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,13 +10,18 @@ export class HomePage {
 
   tasks: Array<{cliente: string, date: any}>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public modalCtrl: ModalController) {
      this.tasks = [
       { cliente: 'Taina', date:  new Date("10/10/2016 10:13:00")},
       { cliente: 'Neusa', date:  new Date("10/10/2016 10:13:00") },
       { cliente: 'Renata', date:  new Date("10/10/2016 10:13:00") },
       { cliente: 'Ide', date:  new Date("10/10/2016 10:13:00") }
     ];
+  }
+
+   openModal() {
+    let modal = this.modalCtrl.create(NewItemModal);
+    modal.present();
   }
 
 }
--- src/pages/prices/new.ts
+++ src/pages/prices/new.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ViewController} from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
 
 @Component({
   templateUrl: 'new.html'
 })
 
 export class NewItemModal {
+  formItem : any = {};
 
-  constructor(public viewCtrl: ViewController) {}
+  constructor(public viewCtrl: ViewController,public fb: FormBuilder, public toastCtrl: ToastController) {
+      this.formItem = this.fb.group({
+          name:['',Validators.required],
+          price:['',Validators.required]
+      })
+  }
 
-  submit(itemInfo) {
-    // Instantiate and save when the form is submitted
-    console.log(itemInfo);
+  submit() {
+    console.log(this.formItem.value);
     this.viewCtrl.dismiss();
-    //this.item = new Item(itemInfo);
-    //this.item.save();
+     let toast = this.toastCtrl.create({
+      message: 'Item cadastrado com sucesso',
+      duration: 3000
+    });
+    toast.present();
   }
   dismiss() {
     this.viewCtrl.dismiss();
